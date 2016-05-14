96cccb4aa86ce62c6a10103373bca47373c68ddc
weixianwei <weixianwei4@126.com>
2016-05-14 13:38:01 +0800

    添加所有页面

--- src/AppBundle/Controller/NewPageController.php
+++ src/AppBundle/Controller/NewPageController.php
@@ -24,6 +24,21 @@ class NewPageController extends BaseController
         return $this->render('AppBundle:newPage:study.html.twig');
     }
 
+    public function photoAction()
+    {
+        return $this->render('AppBundle:newPage:photo.html.twig');
+    }
+
+    public function singleAction()
+    {
+        return $this->render('AppBundle:newPage:single.html.twig');
+    }
+
+    public function travelAction()
+    {
+        return $this->render('AppBundle:newPage:travel.html.twig');
+    }
+
     protected function getNewPageService()
     {
         return $this->getServiceKernel()->createService('AppBundle:NewPage.NewPageService');
