f13d6fe609a5f1f17ee88e670fcb7d2a02ed6eba
尉贤伟 <wxw@zlzkj.com>
2016-11-05 19:52:43 +0800

    fix

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -3,12 +3,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\Security\Http\SecurityEvents;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Service\Common\ServiceKernel;
-//use Topxia\Service\User\CurrentUser;
-//use Topxia\Service\Common\AccessDeniedException;
 
 abstract class BaseController extends Controller
 {
