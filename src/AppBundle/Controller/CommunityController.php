502a33091bc5723c296cc55def15bebf3f32131d
尉贤伟 <wxw@zlzkj.com>
2016-11-09 00:17:00 +0800

    activity

--- src/AppBundle/Controller/CommunityController.php
+++ src/AppBundle/Controller/CommunityController.php
@@ -10,12 +10,19 @@ namespace AppBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommunityController extends Controller
 {
-    public function memberAction()
+    public function memberAction(Request $request)
     {
         return $this->render('AppBundle:Community:member.html.twig', array(
+        ));
+    }
+
+    public function activityAction(Request $request)
+    {
+        return $this->render('AppBundle:Community:activity.html.twig', array(
         ));
     }
 }
